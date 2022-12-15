04302735e3c2c657a901aa97c4cf3bc966c5626c
whyaxee <90753483+whyaxee@users.noreply.github.com>
2022-12-15 12:29:59 +0800

    rename exercises

--- exercise/exercise_8/exercise_8.ino
+++ exercise/exercise_8/exercise_8.ino
@@ -1,38 +1,73 @@
+// Include the libraries we need
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include <smorphi.h>
 
+// Data wire is plugged into port 1 on the Masterboard
+#define ONE_WIRE_BUS 16
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Initialise the robot
 Smorphi my_robot;
 
-void setup() {
+/*
+ * The setup function. We only start the sensors here
+ */
+void setup(void)
+{
+  // start serial port
   Serial.begin(115200);
-  my_robot.BeginSmorphi();
+  Serial.println("Dallas Temperature IC Control Library Demo");
+
+  // Start up the library
+  sensors.begin();
 }
 
-void loop() {
-    int sensor1_status = my_robot.module1_sensor_status(0);
-    int sensor2_status = my_robot.module2_sensor_status(4);
-    int sensor3_status = my_robot.module3_sensor_status(0);
-//    Serial.println("***********");
-//    Serial.println(sensor1_status);
-//    Serial.println(sensor3_status);
-//    Serial.println("***********");
-    if (sensor2_status == 1){
-      if (sensor1_status == 0 && sensor3_status == 0){
-        my_robot.MoveForward(10);
-      }
-      else if (sensor1_status == 1 && sensor3_status == 0){
-        my_robot.CenterPivotLeft(10);
-      }
-      else if (sensor1_status == 0 && sensor3_status == 1){
-        my_robot.CenterPivotRight(10);
-      }
-      
+/*
+ * Main function, get and show the temperature
+ */
+void loop(void)
+{ 
+  // call sensors.requestTemperatures() to issue a global temperature 
+  // request to all devices on the bus
+  Serial.print("Requesting temperatures...");
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.println("DONE");
+  // After we got the temperatures, we can print them here.
+  // We use the function ByIndex, and as an example get the temperature from the first sensor only.
+  float tempC = sensors.getTempCByIndex(0);
+
+  // Check if reading was successful
+  if(tempC != DEVICE_DISCONNECTED_C) 
+  {
+    Serial.print("Temperature for the device 1 (index 0) is: ");
+    Serial.println(tempC);
+    
+    // if temperature is more than 30°C, Smorphi moves forward
+    if (tempC > 30) 
+    {
+      my_robot.MoveForward(10);
+    }
+    // if temperature is less than 30°C, Smorphi stops
+    else
+    {
       my_robot.sm_reset_M1();
       my_robot.sm_reset_M2();
       my_robot.sm_reset_M3();
       my_robot.sm_reset_M4();
-    }
-
-    else {
       my_robot.stopSmorphi();
     }
+  } 
+  
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
+
+  
 }
